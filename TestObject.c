15fdf7681c0379ecae5874318eea5e7e8bfedc02
IceColdCode <IceCodeDev@gmail.com>
2015-05-26 15:09:23 -0600

    Create TestObject.c

---
+++ TestObject.c
@@ -0,0 +1,17 @@
+#include <objects.h>
+#include <stdio.h>
+#include <stdlib.h>
+// members
+char[] test = {"Hello World"} 
+
+// methods
+void printMsg(char[] msg) {
+  
+  printf(msg);
+  
+}
+
+// class definition
+class *TestObject;
+
+TestObject.addMethod(&printMsg(test));
