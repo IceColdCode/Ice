47d64c96a3750831a831c549833a6fa2acbe5281
IceColdCode <IceCodeDev@gmail.com>
2015-05-26 14:59:13 -0600

    Update and rename macro/charcodes.h to objects.h

---
+++ objects.h
@@ -0,0 +1,14 @@
+#ifndef OBJECTS_H_INCLUDED
+
+typedef struct {
+  
+  int numberOfMethods;
+  int *methods[numberOfMethods];
+  bool hasMethods = false;
+  int numberOfMembers;
+  int *members[numberOfMembers]
+  bool hasMembers = false;
+  
+}class;
+
+#endif // OBLECTS_H_INCLUDED
